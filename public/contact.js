e6af8bbd54e28dc7e524eb2cbc7d0de1d173fbf0
MerrilBds <Bdsmerril@gmail.com>
2023-08-25 14:12:23 +0200

    update

--- public/contact.js
+++ public/contact.js
@@ -1,24 +1,38 @@
-function sendMail() {
-    var params = {
-        name: document.getElementById("name").value,
-        email: document.getElementById("email").value,
-        message: document.getElementById("message").value,
-    };
+(function () {
+ 
 
-    const serviceID = "service_t8vbv71";
-    const templateID = "template_71vxhro";
+    const btn = document.getElementById('button');
 
-    emailjs.send(serviceID, templateID, params)
-        .then(res => {
-            document.getElementById("name").value = "";
-            document.getElementById("email").value = "";
-            document.getElementById("message").value = "";
-            console.log(res);
-            alert("Your message sent successfully!!")
+    document.getElementById('form')
+        .addEventListener('submit', function (event) {
+            event.preventDefault();
 
-        })
-        .catch(err => console.log(err));
+            btn.value = 'Envoie...';
 
-}
-   
+            const serviceID = 'service_a8ye8rf';
+            const templateID = 'template_5xw2s6b';
 
+            // Récupére les valeurs des champs du formulaire
+            const name = document.getElementById('name').value;
+            const email = document.getElementById('email').value;
+            const subject = document.getElementById('subject').value;
+            const message = document.getElementById('message').value;
+
+            // Remplace les variables dans le template emailjs
+            const templateParams = {
+                name: name,
+                subject:subject,
+                email: email,
+                message: message
+            };
+
+            emailjs.send(serviceID, templateID, templateParams)
+                .then(() => {
+                    btn.value = 'Envoyer';
+                    alert('Envoyé avec succès!');
+                }, (err) => {
+                    btn.value = 'Envoyer';
+                    alert(JSON.stringify(err));
+                });
+        });
+})();
